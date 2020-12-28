a820f1107a52aa9d3adec9b21853c93c0c0e9c95
Nithur-M <49608085+Nithur-M@users.noreply.github.com>
2020-12-28 23:47:46 +0530

    Chart functionality integrated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-//import SensorDropdown from '../src/components/sensorDropdown';
-import Nav from './components/nav.component';
 
+
+import Nav from './components/nav.component';
 import Home from "./components/home.component";
 import Login from "./components/login.component";
 import SignUp from "./components/signup.component";
@@ -22,7 +23,6 @@ export default class App extends Component {
 
     render() {
         return (
-        //  <SensorDropdown/>
         <Router>
           <div className="App">
             <Nav user={this.state.user} setUser={this.setUser}/>
---
+++ src/services/sensorMock.js
@@ -0,0 +1,19 @@
+import axiosInstance from './axios';
+
+export const fetchSensorMock = async ( sensor ) => {
+    console.log(sensor);
+    if(!sensor){
+        sensor = 'T004';
+    }
+    try {
+        const { data } = await axiosInstance.get('/sensorsmock/send_sensor_reads_to_charts', { params: { sensor_id: sensor }});
+
+        const modifiedData = data.map((sensorMock) => ({
+            data_value: sensorMock.data_value,
+            date: sensorMock.date,
+        }));
+        console.log(modifiedData.data_value);
+        return modifiedData;
+    } catch(err){
+
+    }}
